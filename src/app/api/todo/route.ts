6739d3d7ce586b4ee22371762f14ddeeefe91637
mfghir <fatemeghafari77@gmail.com>
2023-09-04 23:10:34 -0700

    write api

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,15 +1,63 @@
 import clientPromise from "@/lib/mongoDB";
 import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
- 
-
 
 export async function GET() {
-    const client = await clientPromise
-    const collection = client.db().collection('todo')
-    try{
-        const todos = await collection.find({}).toArray()
-        return NextResponse.json(todos ,{status: 200})
+  const client = await clientPromise;
+  const collection = client.db().collection("todos");
 
-    }
+  try {
+    const todos = await collection.find({}).toArray();
+    return NextResponse.json(todos, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const client = await clientPromise;
+  const collection = client.db().collection("todos");
+  const { text } = await req.json();
+
+  try {
+    const todo = { text: text, completed: false };
+    await collection.insertOne(todo);
+    return NextResponse.json(todo, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  const { id, text, completed } = await req.json();
+  const client = await clientPromise;
+  const collection = client.db().collection("todos");
+  try {
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: { text, completed } }
+    );
+
+    return NextResponse.json(
+      { message: "Todo update successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  const { id } = await req.json();
+  const client = await clientPromise;
+  const collection = client.db().collection("todos");
+  try {
+    await collection.deleteOne({ _id: new ObjectId(id) });
+    return NextResponse.json(
+      { message: "Todo Deleted Successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
 }
